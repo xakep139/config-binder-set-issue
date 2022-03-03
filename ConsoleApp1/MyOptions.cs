4c7c55080a5682fd716c55b57189372208b7d28d
Nikita Balabaev <nbalabaev@microsoft.com>
2022-03-03 14:26:49 +0100

    Add project files.

---
+++ ConsoleApp1/MyOptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1;
+
+public class MyOptions
+{
+    // This works:
+    public ISet<string> AssignedSet { get; set; } = new HashSet<string>();
+
+    // This works:
+    public HashSet<string> AssignedHashSet { get; set; } = new HashSet<string>();
+
+    // This works:
+    public HashSet<string>? NullableHashSet { get; set; }
+
+    // This causes an exception:
+    //public ISet<string>? NullableSet { get; set; }
+
+    // This works:
+    public IDictionary<string, HashSet<string>> AssignedDict { get; set; } = new Dictionary<string, HashSet<string>>();
+
+    // This works:
+    public IDictionary<string, HashSet<string>>? NullableDict { get; set; }
+
+    // This causes an exception:
+    //public IDictionary<string, ISet<string>> AssignedISetDict { get; set; } = new Dictionary<string, ISet<string>>();
+
+    // This causes an exception:
+    //public IDictionary<string, ISet<string>>? NullableISetDict { get; set; }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var host = Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(static (context, services)
+                => services.Configure<MyOptions>(context.Configuration.GetSection("MyOptions")))
+            .Build();
+
+        var options = host.Services.GetRequiredService<IOptions<MyOptions>>();
+        var value = options.Value;
+        Console.WriteLine("AssignedSet element count: {0}", value.AssignedSet.Count);
+        Console.WriteLine("AssignedHashSet element count: {0}", value.AssignedHashSet.Count);
+        Console.WriteLine("NullableHashSet element count: {0}", value.NullableHashSet?.Count);
+        Console.WriteLine("AssignedDict element count: {0}", value.AssignedDict.Count);
+        Console.WriteLine("NullableDict element count: {0}", value.NullableDict?.Count);
+    }
+}
